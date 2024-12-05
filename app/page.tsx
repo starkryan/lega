bc0dfb779184dd298618765db1a055789f0bdb74
Neo <dixitrayaan@gmail.com>
2024-12-05 12:41:18 +0530

    Lega

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col items-center mt-20 space-y-4">
        <a
-        href="https://wa.me/1234567890"
+        href="https://wa.me/447546629150"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-4 py-2 border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-lg transition duration-300"
@@ -15,7 +15,7 @@ const LandingPage = () => {
       </a>
       <a
         
-        href="https://wa.me/1234567890"
+        href="https://wa.me/447546629150"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-4 py-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white rounded-lg transition duration-300"
