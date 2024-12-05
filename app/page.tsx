74aef1ca40941bc2c6dfa5c61a19d894d58ba3e0
Neo <dixitrayaan@gmail.com>
2024-12-05 22:08:59 +0530

    why

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col items-center mt-20 space-y-4">
        <a
-        href="https://wa.me/447546629150"
+        href="https://wa.me/16832251798"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-4 py-2 border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-lg transition duration-300"
@@ -16,7 +16,7 @@ const LandingPage = () => {
       </a>
       <a
         
-        href="https://wa.me/447546629150"
+        href="https://wa.me/16832251798"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-4 py-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white rounded-lg transition duration-300"
